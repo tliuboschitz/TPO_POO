2b86ec954f108ed9d366100a1532807c0b58a767
tliuboschitz <tliuboschitz@uade.edu.ar>
2025-11-18 17:34:40 -0300

    correcion de bugs

--- TPO/Reserva.java
+++ TPO/Reserva.java
@@ -3,10 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.UUID;
 
 public class Reserva {
     private int idReserva;
@@ -18,7 +15,6 @@ public class Reserva {
     private double monto;
     private String estado; // "Pendiente", "Confirmada", "Cancelada"
 
-<<<<<<< HEAD
     public Reserva(java.util.Date fecha2, String hora, Alquilador alquilador, Cancha cancha) {
         this.idReserva = proximoId++;
         // Incrementa el ID para la próxima reserva, se reinicia cuando se abre el programa
@@ -31,22 +27,6 @@ public class Reserva {
         this.estado = "Pendiente";
     }
         
-=======
-        public Reserva(java.util.Date fecha, String hora, Alquilador alquilador, Cancha cancha) {
-            this.idReserva = proximoId++;
-            // Incrementa el ID para la próxima reserva, se reinicia cuando se abre el programa
-            // pero por lo menos los IDs son únicos durante la ejecución y son simples y legibles.
-            this.fecha = fecha;
-            this.hora = hora;
-            this.alquilador = alquilador;
-            this.cancha = cancha;
-            this.monto = cancha.getPrecioHora();
-            this.estado = "Pendiente";
-            addTablaR(alquilador.getDni(), cancha.getIdCancha());
-
-        }
-
->>>>>>> origin/main
         public int getIdReserva() {
             return idReserva;
         }
